429a624eed108ede0ef2962c5f3ef48b983f34ed
LZW <1312299240@qq.com>
2019-08-19 20:17:45 +0800

    s2pactice1

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result = [];
+ var m = new Map();
+  for (var i=0;i<collection.length;i++){
+    if (m.get(collection[i])==null){
+      m.set(collection[i],1);
+    }else{
+      m.set(collection[i],m.get(collection[i])+1);
+    }
+  }
+  //console.log(map);
+  m.forEach(function(key,value){
+    result.push({key:value,count:key})
+  });
+  console.log(result);
+  return result;
 }
